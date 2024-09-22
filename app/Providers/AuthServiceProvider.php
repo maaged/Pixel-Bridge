74ea60924df06716ab06cd98bdcd608ca5b573f4
maaged <maagedashour@yahoo.com>
2024-09-22 05:56:41 +0300

    new 99

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Passport\Passport;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        //
+         'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *@return void
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+        Gate::before(function ($user, $ability) {
+            return $user->id == 1;
+        });
+
+
+    }
+
+public function register()
+{
+   Passport::ignoreRoutes();
+}
+}
